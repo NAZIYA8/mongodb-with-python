3e466268a9e21626c50756853508526f153a7dce
Naziya <naziyasyeda11@gmail.com>
2021-08-10 00:26:56 +0530

    [Naziya]Add: logger format file and created collection student

---
+++ CRUD_operations/crud_operations.py
@@ -0,0 +1,28 @@
+'''
+@Author: Naziya
+@Date: 2021-08-09
+@Last Modified by: Naziya
+@Last Modified : 2021-08-09
+@Title : Program Aim is to perform CRUD operations.
+'''
+
+from LoggerFormat import logger
+from pymongo import MongoClient
+client = MongoClient('localhost',27017)
+
+db = client.test
+
+def create_collection():
+    try:
+        print("Creating collection students.")
+        col = db.students
+        if col.drop():
+            print('Deleted existing collection')
+        db.create_collection("students")
+    except Exception as err:
+        logger.error(err)
+
+
+if __name__ == "__main__":
+    create_collection()
+    
