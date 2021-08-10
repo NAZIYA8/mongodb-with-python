1d584e9ad151b9072a4191f9cf376717b624317b
Naziya <naziyasyeda11@gmail.com>
2021-08-11 04:07:51 +0530

    [Naziya]Add:inserted multiple documents into collection

--- Aggregate_functions/aggregate_functions.py
+++ Aggregate_functions/aggregate_functions.py
@@ -26,8 +26,27 @@ def create_collection():
     except Exception as err:
         logger.error(err)
 
+def insert_multiple_doc():
+    """
+        Description: 
+            This function is used to insert multiple documents to 
+            the collection.
+    """
+    try:
+        db.student.insert_many([
+            {'user':'Kinjal',"subject":'Database','score':80},
+            {'user':'Dolly',"subject":'Javascript','score':90},
+            {'user':'Dolly',"subject":'Database','score':85},
+            {'user':'Kinjal',"subject":'Javascript','score':75},
+            {'user':'Dolly',"subject":'DataScience','score':60},
+            {'user':'Kinjal',"subject":'DataScience','score':95}
+        ])
+        print("Inserted multiple documents\n")
+    except Exception as err:
+        logger.error(err)
+
 
 if __name__ == "__main__":
     
     create_collection()
-    
+    insert_multiple_doc()
