4a627b6a2ab28e33454836957549cb40e79f00ae
Naziya <naziyasyeda11@gmail.com>
2021-08-11 04:06:29 +0530

    [Naziya]Add:created a database and a collection

---
+++ Aggregate_functions/aggregate_functions.py
@@ -0,0 +1,33 @@
+'''
+@Author: Naziya
+@Date: 2021-08-10
+@Last Modified by: Naziya
+@Last Modified : 2021-08-10
+@Title : Program Aim is to perform sort operations.
+'''
+
+from LoggerFormat import logger
+from pymongo import MongoClient
+
+from LoggerFormat import logger
+from pymongo import MongoClient
+client = MongoClient('localhost',27017)
+
+db = client.school
+
+
+def create_collection():
+    try:
+        print("Creating collection student.")
+        col = db.student
+        if col.drop():
+            print('Deleted existing collection')
+        db.create_collection("student")
+    except Exception as err:
+        logger.error(err)
+
+
+if __name__ == "__main__":
+    
+    create_collection()
+    
