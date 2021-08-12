c1317bb6754f0f21823254fec7eff9ca9a4a3c03
Naziya <naziyasyeda11@gmail.com>
2021-08-13 02:06:34 +0530

    [Naziya]Add: inserted documents into the collection

--- MapReduce/map_reduce.py
+++ MapReduce/map_reduce.py
@@ -23,10 +23,33 @@ def create_collection():
     except Exception as err:
         logger.error(err)
 
+def insert_multiple_doc():
+    """
+        Description: 
+            This function is used to insert multiple documents to 
+            the collection.
+    """
+    try:
+        db.orders.insert_many([
+            { '_id': 1, 'cust_id': "Kinjal",  'price': 25, 'items': [ { 'sku': "oranges", 'qty': 5, 'price': 2.5 }, { 'sku': "apples", 'qty': 5, 'price': 2.5 } ]},
+            { '_id': 2, 'cust_id': "Kinjal",  'price': 70, 'items': [ { 'sku': "oranges", 'qty': 8, 'price': 2.5 }, { 'sku': "chocolates", 'qty': 5, 'price': 10 } ]},
+            { '_id': 3, 'cust_id': "Dolly",  'price': 50, 'items': [ { 'sku': "oranges", 'qty': 10, 'price': 2.5 }, { 'sku': "pears", 'qty': 10, 'price': 2.5 } ] },
+            { '_id': 4, 'cust_id': "Dolly",  'price': 25, 'items': [ { 'sku': "oranges", 'qty': 10, 'price': 2.5 } ] },
+            { '_id': 5, 'cust_id': "Dolly",  'price': 50, 'items': [ { 'sku': "chocolates", 'qty': 5, 'price': 10 } ]},
+            { '_id': 6, 'cust_id': "Samar", 'price': 35, 'items': [ { 'sku': "carrots", 'qty': 10, 'price': 1.0 }, { 'sku': "apples", 'qty': 10, 'price': 2.5 } ]},
+            { '_id': 7, 'cust_id': "Samar",  'price': 25, 'items': [ { 'sku': "oranges", 'qty': 10, 'price': 2.5 } ]},
+            { '_id': 8, 'cust_id': "Pranay", 'price': 75, 'items': [ { 'sku': "chocolates", 'qty': 5, 'price': 10 }, { 'sku': "apples", 'qty': 10, 'price': 2.5 } ] },
+            { '_id': 9, 'cust_id': "Pranay",  'price': 55, 'items': [ { 'sku': "carrots", 'qty': 5, 'price': 1.0 }, { 'sku': "apples", 'qty': 10, 'price': 2.5 }, { 'sku': "oranges", 'qty': 10, 'price': 2.5 } ]},
+            { '_id': 10, 'cust_id': "Pranay",  'price': 25, 'items': [ { 'sku': "oranges", 'qty': 10, 'price': 2.5 } ] }
+                ])
+        print("Inserted multiple documents\n")
+    except Exception as err:
+        logger.error(err)
 
 
 if __name__ == "__main__":
     
     create_collection()
+    insert_multiple_doc()
     
   
