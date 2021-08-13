10c0029e23b7a636d3dd40a04801b60d0ca69680
Naziya <naziyasyeda11@gmail.com>
2021-08-13 23:09:53 +0530

    [Naziya]Add:Inserted multiple documents into the collection

--- import_export/import_export.py
+++ import_export/import_export.py
@@ -22,3 +22,12 @@ if col.drop():
     logger.info('Deleted existing collection')
 db.create_collection("emp_details")
 
+db.emp_details.insert_many([
+        {"EmployeeNo" : "1","FirstName" : "Dolly","LastName": "Singh","Age" : "27","Salary": "30000",'Gender':'Female'},
+        {"EmployeeNo" : "2","FirstName" : "Amit","LastName": "Sharma","Age" : "40","Salary": "50000",'Gender':'Male'},
+        {"EmployeeNo" : "3","FirstName" : "Komal","LastName": "Pande","Age" : "20","Salary": "18000",'Gender':'Female'},
+        {"EmployeeNo" : "4","FirstName" : "Ashima","LastName": "Arora","Age" : "25","Salary": "28000",'Gender':'Female'},
+        {"EmployeeNo" : "5","FirstName" : "Sneha","LastName": "Joshi","Age" : "30","Salary": "45000",'Gender':'Female'},
+        {"EmployeeNo" : "6","FirstName" : "Gautham","LastName": "Deshpande","Age" : "30","Salary": "40000",'Gender':'Male'}
+        ])
+logger.info("Inserted Multiple Documents\n")
