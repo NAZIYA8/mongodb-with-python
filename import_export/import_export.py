fcda4630dac97a92c25f010c0980c7685732907c
Naziya <naziyasyeda11@gmail.com>
2021-08-13 23:37:47 +0530

    [Naziya]Add: exported the mongodb documents to html file

--- import_export/import_export.py
+++ import_export/import_export.py
@@ -16,52 +16,72 @@ import json
 client = MongoClient('localhost',27017)
 db = client.test3
 
-logger.info("Creating collection emp_details")
-col = db.emp_details
-if col.drop(): 
-    logger.info('Deleted existing collection')
-db.create_collection("emp_details")
+try:
+    logger.info("Creating collection emp_details")
+    col = db.emp_details
+    if col.drop(): 
+        logger.info('Deleted existing collection')
+    db.create_collection("emp_details")
 
-db.emp_details.insert_many([
-        {"EmployeeNo" : "1","FirstName" : "Dolly","LastName": "Singh","Age" : "27","Salary": "30000",'Gender':'Female'},
-        {"EmployeeNo" : "2","FirstName" : "Amit","LastName": "Sharma","Age" : "40","Salary": "50000",'Gender':'Male'},
-        {"EmployeeNo" : "3","FirstName" : "Komal","LastName": "Pande","Age" : "20","Salary": "18000",'Gender':'Female'},
-        {"EmployeeNo" : "4","FirstName" : "Ashima","LastName": "Arora","Age" : "25","Salary": "28000",'Gender':'Female'},
-        {"EmployeeNo" : "5","FirstName" : "Sneha","LastName": "Joshi","Age" : "30","Salary": "45000",'Gender':'Female'},
-        {"EmployeeNo" : "6","FirstName" : "Gautham","LastName": "Deshpande","Age" : "30","Salary": "40000",'Gender':'Male'}
-        ])
-logger.info("Inserted Multiple Documents\n")
+    db.emp_details.insert_many([
+            {"EmployeeNo" : "1","FirstName" : "Dolly","LastName": "Singh","Age" : "27","Salary": "30000",'Gender':'Female'},
+            {"EmployeeNo" : "2","FirstName" : "Amit","LastName": "Sharma","Age" : "40","Salary": "50000",'Gender':'Male'},
+            {"EmployeeNo" : "3","FirstName" : "Komal","LastName": "Pande","Age" : "20","Salary": "18000",'Gender':'Female'},
+            {"EmployeeNo" : "4","FirstName" : "Ashima","LastName": "Arora","Age" : "25","Salary": "28000",'Gender':'Female'},
+            {"EmployeeNo" : "5","FirstName" : "Sneha","LastName": "Joshi","Age" : "30","Salary": "45000",'Gender':'Female'},
+            {"EmployeeNo" : "6","FirstName" : "Gautham","LastName": "Deshpande","Age" : "30","Salary": "40000",'Gender':'Male'}
+            ])
+    logger.info("Inserted Multiple Documents\n")
 
-result = db.emp_details.find()
-for data in result:
-    logger.info(data)
-logger.info("Showed all documents\n")
+    result = db.emp_details.find()
+    for data in result:
+        logger.info(data)
+    logger.info("Showed all documents\n")
 
-cursor = col.find()
-mongo_docs = list(cursor)
+    cursor = col.find()
+    mongo_docs = list(cursor)
 
-logger.info("total docs:")
-logger.info(len(mongo_docs))
+    logger.info("total docs:")
+    logger.info(len(mongo_docs))
 
-docs = pandas.DataFrame(columns=[])
-for num, doc in enumerate(mongo_docs): 
-    doc_id = str(doc["_id"])
-    doc["_id"]=doc_id
-    series_obj = pandas.Series( doc, name=doc_id )
-    docs = docs.append( series_obj )
+    docs = pandas.DataFrame(columns=[])
+    for num, doc in enumerate(mongo_docs): 
+        doc_id = str(doc["_id"])
+        doc["_id"]=doc_id
+        series_obj = pandas.Series( doc, name=doc_id )
+        docs = docs.append( series_obj )
 
-# export MongoDB documents to a json file
-docs.to_json("object_rocket.json",orient='records')
+    # export MongoDB documents to a json file
+    docs.to_json("object_rocket.json",orient='records')
 
-# export MongoDB documents to json
-json_export = docs.to_json(orient='records') 
-logger.info("\nJSON data:")
-logger.info(json_export)
+    # export MongoDB documents to json
+    json_export = docs.to_json(orient='records') 
+    logger.info("\nJSON data:")
+    logger.info(json_export)
 
-# export MongoDB documents to a CSV file
-docs.to_csv("object_rocket.csv", ",") # CSV delimited by commas
+    # export MongoDB documents to a CSV file
+    docs.to_csv("object_rocket.csv", ",")
+    # export MongoDB documents to CSV
+    csv_export = docs.to_csv(sep=",") 
+    logger.info("\nCSV data:")
+    logger.info(csv_export)
 
-# export MongoDB documents to CSV
-csv_export = docs.to_csv(sep=",") # CSV delimited by commas
-logger.info("\nCSV data:")
-logger.info(csv_export)
+
+    # create IO HTML string
+    import io
+    html_str = io.StringIO()
+
+    # export as HTML
+    docs.to_html(
+    buf=html_str,
+    classes='table table-striped'
+    )
+
+    # print out the HTML table
+    logger.info(html_str.getvalue())
+
+    # save the MongoDB documents as an HTML table
+    docs.to_html("object_rocket.html")
+
+except Exception as err:
+        logger.error(err)
